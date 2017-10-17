ceae213a12db354f91c2ce3cae47dcd6b09b6ca7
Nelestya <tristand.pro@gmail.com>
2017-10-17 18:24:14 +0200

    change the app name

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import Order_Create, Admin_Order_Detail, Admin_Order_Pdf
 from django.contrib.admin.views.decorators import staff_member_required
 
-app_name = 'orders'
+app_name = 'statistiques'
 urlpatterns = [
     url(r'^admin/order/(?P<ip>\d+)/$', staff_member_required(Admin_Stat_Ip.as_view()), name='admin_ip_detail'),
 ]
