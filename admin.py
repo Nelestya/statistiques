3709026debd42ec00c500a293186237896f40af0
Nelestya <tristand.pro@gmail.com>
2017-10-17 18:25:29 +0200

    change the url

--- admin.py
+++ admin.py
@@ -7,9 +7,8 @@ from django.core.urlresolvers import reverse
 # Register your models here.
 
 def stats_ip_page(obj):
-    return '<a href="{}">ListPage</a>'.format(reverse('statistique:admin_ip_detail', args=[obj.id]))
+    return '<a href="{}">ListPage</a>'.format(reverse('statistiques:admin_ip_detail', args=[obj.id]))
 order_detail.allow_tags = True
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['ip', stats_ip_page]
-    
